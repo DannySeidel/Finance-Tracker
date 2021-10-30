b32421756496617d37858f56214a2b888c409802
Danny Seidel <danny.seidel@porsche.digital>
2021-10-30 13:34:04 +0200

    Initial Commit

---
+++ Finance Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Finance Tracker
+//
+//  Created by Danny Seidel on 30.10.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Finance Tracker/Finance_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_TrackerApp.swift
+//  Finance Tracker
+//
+//  Created by Danny Seidel on 30.10.21.
+//
+
+import SwiftUI
+
+@main
+struct Finance_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
