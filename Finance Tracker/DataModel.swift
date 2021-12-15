8c8037b4efaab3621acd4586ccbb53babdf6b09d
Danny Seidel <danny.seidel@porsche.digital>
2021-12-15 21:43:49 +0100

    clean project folder

--- Finance Tracker/DataModel.swift
+++ Finance Tracker/DataModel.swift
@@ -7,26 +7,128 @@
 
 import Foundation
 import SwiftUI
+import SQLite
 
-struct DataStructure: Hashable {
+
+struct Transaction: Hashable, Identifiable {
+    var id = UUID().uuidString
     var amount: Double
     var name: String
     var category: String
-    var date: Date
-    var repeatT: Int
-//    var enddate: Date
-//    var coordinates: Coordinates
-    
-    struct Coordinates: Hashable {
-        var latitude: Double
-        var longitude: Double
-    }
+    var dateAndTime: Date
+    var repeatTag: Int
+    var endRepeat: Bool
+    var repeatEndDate: Date
 }
 
-class TransactionData: ObservableObject {
-    @Published var transaction: [DataStructure] = []
+
+struct HistoryTransaction: Hashable, Identifiable {
+    var id: String
+    var amount: Double
+    var name: String
+    var category: String
+    var dateAndTime: Date
+}
+
+
+struct Amount: Hashable {
+    var amount: Double
+}
+
+
+struct Category: Hashable {
+    var name: String
+}
+
+
+class Data: ObservableObject {
     
-    func addTransaction(transactionID: Int) {
-        transaction[0] = DataStructure(amount: 34.1, name: "df", category: "food", date: Date.now, repeatT: 2)
+    var database: Database = {
+        var instance = Database()
+        instance.connectToDatabase()
+        instance.createTables()
+        instance.insertDefaults()
+        return instance
+    }()
+    
+    @Published var balance = 0.0
+    
+    @Published var transactionGroups: [[HistoryTransaction]] = [[]]
+
+    init() {
+        refreshBalance()
+        refreshTransactionGroups()
     }
+    
+    func refreshBalance() {
+        balance = database.getMonthlyBalance()
+    }
+    
+    func refreshTransactionGroups() {
+        var groups: [[HistoryTransaction]] = []
+        for transaction in database.getTransactionsForHistory() {
+            var groupexists = false
+            for group in groups {
+                if let firstGroup = group.first, Calendar.current.isDate(
+                    firstGroup.dateAndTime, inSameDayAs: transaction.dateAndTime
+                ) {
+                    let groupIndex = groups.firstIndex(of: group)!
+                    var newGroup = groups[groupIndex]
+                    newGroup.append(transaction)
+                    groups[groupIndex] = newGroup
+                    groupexists = true
+                }
+            }
+            if !groupexists {
+                groups.append([transaction])
+            }
+        }
+        transactionGroups = groups
+    }
+    
+    @Published var categoriesExpense = [
+        "Car",
+        "Clothes",
+        "Computers",
+        "Entertainment",
+        "Freetime",
+        "Food & Drinks",
+        "Gifts",
+        "Groceries",
+        "Health",
+        "Household",
+        "Rent",
+        "Restaurants & Cafes",
+        "Transport"
+    ]
+    
+    @Published var categoriesIncome = [
+        "Business Income",
+        "Salary",
+        "Stock Market",
+        "Tax Refunds"
+    ]
+    
+    @Published var names = [
+        "Breakfest",
+        "Lunch",
+        "Coffee",
+        "Dinner",
+        "Theather",
+        "Amusement Park",
+        "Aldi",
+        "Edeka",
+        "Lidl",
+        "tegut",
+        "Netto",
+        "Norma",
+        "Penny",
+        "Car",
+        "Bike",
+        "E-Scooter",
+        "Public Transport",
+        "Taxi",
+        "Train",
+        "Airplane",
+    ]
 }
