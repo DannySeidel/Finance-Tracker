DannySeidel/Finance-Tracker

commit c313850b4557ea90f21b973bb5632e7e8d94b97f

added alert if amount missing + no name/category now possible

Finance Tracker/Views/HistoryView/EditTransactionView.swift
@@ -22,6 +22,8 @@ struct EditTransactionView: View {
     @Binding var uuid: String
     @Binding var transactionType: Bool
     
+    @State private var showingAlert = false
+    
     @AppStorage("storeNewCategoriesByDefault") var storeNewCategoriesByDefault = true
     
     var transaction: Transaction {
@@ -62,24 +64,37 @@ struct EditTransactionView: View {
                         },
                     trailing:
                         Button("Save") {
-                            data.database.updateTransaction(
-                                transaction: Transaction(
-                                    id: uuid,
-                                    amount: amount! * factor,
-                                    name: name!,
-                                    category: category!,
-                                    dateAndTime: dateAndTime,
-                                    repeatTag: repeatTag,
-                                    endRepeat: endRepeat,
-                                    repeatEndDate: repeatEndDate
+                            if amount != nil {
+                                if name == nil {
+                                    name = "unnamed Transaction"
+                                }
+                                if category == nil {
+                                    category = "no Category"
+                                }
+                                data.database.updateTransaction(
+                                    transaction: Transaction(
+                                        id: uuid,
+                                        amount: amount! * factor,
+                                        name: name!,
+                                        category: category!,
+                                        dateAndTime: dateAndTime,
+                                        repeatTag: repeatTag,
+                                        endRepeat: endRepeat,
+                                        repeatEndDate: repeatEndDate
+                                    )
                                 )
-                            )
-                            if storeNewCategoriesByDefault {
-                                transactionType ? data.database.insertIncomeCategory(newCategory: category!) : data.database.insertExpenseCategory(newCategory: category!)
+                                if storeNewCategoriesByDefault {
+                                    transactionType ? data.database.insertIncomeCategory(newCategory: category!) : data.database.insertExpenseCategory(newCategory: category!)
+                                }
+                                data.refreshTransactionGroups()
+                                data.refreshBalance()
+                                showingSheet.toggle()
+                            } else {
+                                showingAlert.toggle()
                             }
-                            data.refreshTransactionGroups()
-                            data.refreshBalance()
-                            showingSheet.toggle()
+                        }
+                        .alert(isPresented: $showingAlert) {
+                            Alert(title: Text("Missing an amount"))
                         }
                 )
                 .navigationBarTitleDisplayMode(.inline)
@@ -89,7 +104,13 @@ struct EditTransactionView: View {
                 transactionType = transactionType
                 amount = transaction.amount * factor
                 name = transaction.name
+                if name == "unnamed Transaction" {
+                    name = nil
+                }
                 category = transaction.category
+                if category == "no Category" {
+                    category = nil
+                }
                 dateAndTime = transaction.dateAndTime
                 repeatTag = transaction.repeatTag
                 endRepeat = transaction.endRepeat

Finance Tracker/Views/TransactionView/AddTransactionView.swift
@@ -20,6 +20,7 @@ struct AddTransactionView: View {
     @State private var repeatEndDate = Date()
     
     @State private var transactionType = false
+    @State private var showingAlert = false
     
     @AppStorage("storeNewCategoriesByDefault") var storeNewCategoriesByDefault = true
     
@@ -57,7 +58,13 @@ struct AddTransactionView: View {
                         },
                     trailing:
                         Button("Add") {
-                            if (amount != nil) && (name != nil) && (category != nil) {
+                            if (amount != nil) {
+                                if name == nil {
+                                    name = "unnamed Transaction"
+                                }
+                                if category == nil {
+                                    category = "no Category"
+                                }
                                 data.database.insertTransaction(
                                     transaction: Transaction(
                                         amount: amount! * factor,
@@ -72,10 +79,15 @@ struct AddTransactionView: View {
                                 if storeNewCategoriesByDefault {
                                     transactionType ? data.database.insertIncomeCategory(newCategory: category!) : data.database.insertExpenseCategory(newCategory: category!)
                                 }
+                            } else {
+                                showingAlert.toggle()
                             }
                             data.refreshBalance()
                             data.refreshTransactionGroups()
                             showTransactionSheet.toggle()
+                        }
+                        .alert(isPresented: $showingAlert) {
+                            Alert(title: Text("Missing an amount"))
                         }
                 )
                 .navigationBarTitleDisplayMode(.inline)

Finance Tracker/Views/HistoryView/TransactionElement.swift
@@ -28,7 +28,7 @@ struct TransactionElement: View {
                     
                     Spacer()
                     
-                    Text(String(format: "%.2f", transaction.amount))
+                    Text(String(format: "%.2f", transaction.amount) + "€")
                         .foregroundColor(amountcolor)
                 }
                 HStack {

commit 775be0b77d0269864b1f5d6aaab555052e9d4af9

fixed bug introduced in [c313850]

Finance Tracker/Views/HistoryView/EditTransactionView.swift
@@ -83,7 +83,7 @@ struct EditTransactionView: View {
                                         repeatEndDate: repeatEndDate
                                     )
                                 )
-                                if storeNewCategoriesByDefault {
+                                if storeNewCategoriesByDefault && category != "no Category" {
                                     transactionType ? data.database.insertIncomeCategory(newCategory: category!) : data.database.insertExpenseCategory(newCategory: category!)
                                 }
                                 data.refreshTransactionGroups()

Finance Tracker/Views/HistoryView/HistoryView.swift
@@ -88,7 +88,6 @@ struct HistoryView: View {
             trailing:
                 HStack {
                     Text("Filter by:")
-                        .offset(y: -1)
                     Picker("Filter by \(Image(systemName: "shift"))", selection: $filterTag) {
                         Text("Name").tag(0)
                         Text("Category").tag(1)
@@ -96,6 +95,7 @@ struct HistoryView: View {
                         Text("Date").tag(3)
                     }
                     .pickerStyle(MenuPickerStyle())
+                    .offset(y: 1)
                 }
         )
     }

Finance Tracker/Views/TransactionView/AddTransactionView.swift
@@ -76,7 +76,7 @@ struct AddTransactionView: View {
                                         repeatEndDate: repeatEndDate
                                     )
                                 )
-                                if storeNewCategoriesByDefault {
+                                if storeNewCategoriesByDefault && category != "no Category" {
                                     transactionType ? data.database.insertIncomeCategory(newCategory: category!) : data.database.insertExpenseCategory(newCategory: category!)
                                 }
                             } else {