DannySeidel/Finance-Tracker

commit 69e2dcb2f8ade5641dcce37ffc90ef10580f85e2

default timespan now changable

Finance Tracker/DataModel.swift
@@ -42,6 +42,8 @@ struct Category: Hashable {
 
 
 class Data: ObservableObject {
+    @AppStorage("DefaultTimespan") var defaultTimespan: Int = 0
+    
     var database: Database = {
         var instance = Database()
         instance.connectToDatabase()
@@ -60,7 +62,24 @@ class Data: ObservableObject {
     }
     
     func refreshBalance() {
-        balance = database.getMonthlyBalance()
+        switch defaultTimespan {
+        case 1:
+            balance = database.getBalance(startDate: Date().startOfLastMonth(), endDate: Date().endOfLastMonth())
+        case 2:
+            balance = database.getBalance(startDate: Date().todayOneMonthAgo(), endDate: Date())
+        case 3:
+            balance = database.getBalance(startDate: Date().startOfMonthThreeMonthsAgo(), endDate: Date().endOfLastMonth())
+        case 4:
+            balance = database.getBalance(startDate: Date().startOfMonthSixMonthsAgo(), endDate: Date().endOfLastMonth())
+        case 5:
+            balance = database.getBalance(startDate: Date().startOfCurrentYear(), endDate: Date())
+        case 6:
+            balance = database.getBalance(startDate: Date().startOfLastYear(), endDate: Date().endOfLastYear())
+        case 7:
+            balance = database.getBalance(startDate: Date().todayOneYearAgo(), endDate: Date())
+        default:
+            balance = database.getBalance(startDate: Date().startOfCurrentMonth(), endDate: Date())
+        }
     }
     
     func refreshTransactionGroups() {

Finance Tracker/Extensions.swift
@@ -10,7 +10,7 @@ import SQLite
 
 
 extension Date {
-    func startOfMonth() -> Date {
+    func startOfCurrentMonth() -> Date {
         return Calendar.current.date(
             from: Calendar.current.dateComponents(
                 [.year, .month],from: Calendar.current.startOfDay(for: self)
@@ -18,9 +18,65 @@ extension Date {
         )!
     }
     
-    func endOfMonth() -> Date {
+    func endOfCurrentMonth() -> Date {
         return Calendar.current.date(
-            byAdding: DateComponents(month: 1, day: -1), to: self.startOfMonth()
+            byAdding: DateComponents(month: 1, day: -1), to: self.startOfCurrentMonth()
+        )!
+    }
+    
+    func startOfLastMonth() -> Date {
+        return Calendar.current.date(
+            byAdding: DateComponents(month: -1), to: self.startOfCurrentMonth()
+        )!
+    }
+    
+    func endOfLastMonth() -> Date {
+        return Calendar.current.date(
+            byAdding: DateComponents(month: 1, day: -1), to: self.startOfLastMonth()
+        )!
+    }
+    
+    func todayOneMonthAgo() -> Date {
+        return Calendar.current.date(
+            byAdding: DateComponents(month: -1), to: Date()
+        )!
+    }
+    
+    func startOfMonthThreeMonthsAgo() -> Date {
+        return Calendar.current.date(
+            byAdding: DateComponents(month: -3), to: self.startOfCurrentMonth()
+        )!
+    }
+    
+    func startOfMonthSixMonthsAgo() -> Date {
+        return Calendar.current.date(
+            byAdding: DateComponents(month: -6), to: self.startOfCurrentMonth()
+        )!
+    }
+    
+    func startOfCurrentYear() -> Date {
+        return Calendar.current.date(
+            from: Calendar.current.dateComponents(
+                [.year],from: Calendar.current.startOfDay(for: self)
+            )
+        )!
+    }
+    
+    func startOfLastYear() -> Date {
+        return Calendar.current.date(
+            byAdding: DateComponents(year: -1), to: self.startOfCurrentYear()
+        )!
+    }
+    
+    func endOfLastYear() -> Date {
+        return Calendar.current.date(
+            byAdding: DateComponents(year: 1, month: -1), to: self.startOfLastYear()
+        )!
+    }
+    
+    func todayOneYearAgo() -> Date {
+        return Calendar.current.date(
+            byAdding: DateComponents(year: -1), to: Date()
         )!
     }
 }

Finance Tracker/Database.swift
@@ -267,13 +267,13 @@ class Database {
         }
     }
     
-    func getMonthlyBalance() -> Double {
+    func getBalance(startDate: Date, endDate: Date) -> Double {
         var monthlyBalance: Double
         var amounts: [Amount] = []
         do {
             let amountRows = Array(try db.prepare(transactionTable
                                                             .select(amount)
-                                                    .filter(Date().startOfMonth()...Date.now ~= dateAndTime)
+                                                    .filter(startDate...endDate ~= dateAndTime)
                                                          ))
             for amountRow in amountRows {
                 let amount  = Amount.init(row: amountRow)

Finance Tracker/Views/HistoryView/HistoryView.swift
@@ -87,8 +87,9 @@ struct HistoryView: View {
         .navigationBarItems(
             trailing:
                 HStack {
+                    Spacer()
                     Image(systemName: "line.3.horizontal.decrease.circle")
-                    Picker("Filter by \(Image(systemName: "shift"))", selection: $filterTag) {
+                    Picker("\(Image(systemName: "line.3.horizontal.decrease.circle"))", selection: $filterTag) {
                         Text("Name").tag(0)
                         Text("Category").tag(1)
                         Text("Amount").tag(2)

Finance Tracker/Views/MainView/MainView.swift
@@ -12,6 +12,29 @@ struct MainView: View {
     @State private var showSettingsSheet = false
     @EnvironmentObject var data: Data
     
+    @AppStorage("DefaultTimespan") var defaultTimespan: Int = 0
+    
+    var navigationTitle: String {
+        switch defaultTimespan {
+        case 1:
+            return "Previous 30 days"
+        case 2:
+            return "Last Month"
+        case 3:
+            return "Last 3 Months"
+        case 4:
+            return "Last 6 Months"
+        case 5:
+            return "Current Year"
+        case 6:
+            return "Last Year"
+        case 7:
+            return "Previous 365 Days"
+        default:
+            return "Current Month"
+        }
+    }
+    
     var body: some View {
         NavigationView {
             ScrollView {
@@ -42,7 +65,7 @@ struct MainView: View {
                     .foregroundColor(.primary)
                 }
             }
-            .navigationTitle("This Month")
+            .navigationTitle(navigationTitle)
         }
         .sheet(isPresented: $showTransactionSheet) {
             AddTransactionView(showTransactionSheet: $showTransactionSheet)

Finance Tracker/Views/SettingsView/SettingsView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @EnvironmentObject var data: Data
     @AppStorage("storeNewCategoriesByDefault") var storeNewCategoriesByDefault: Bool = true
+    @AppStorage("DefaultTimespan") var defaultTimespan: Int = 0
     
     @Binding var showSettingsSheet: Bool
     
@@ -19,6 +21,16 @@ struct SettingsView: View {
                 NavigationLink(destination: EditCategoryView()) {
                     Text("Edit saved Categories")
                 }
+                Picker("Default Timespan", selection: $defaultTimespan) {
+                    Text("Current Month").tag(0)
+                    Text("Previous 30 Days").tag(1)
+                    Text("Last Month").tag(2)
+                    Text("Last 3 Months").tag(3)
+                    Text("Last 6 Months").tag(4)
+                    Text("Current Year").tag(5)
+                    Text("Last Year").tag(6)
+                    Text("Previous 365 Days").tag(7)
+                }
                 NavigationLink(destination: ManageAnalyticsView()) {
                     Text("Standart Diagram Type")
                 }
@@ -27,6 +39,7 @@ struct SettingsView: View {
             .navigationBarItems(
                 trailing:
                     Button("Done") {
+                        data.refreshBalance()
                         showSettingsSheet.toggle()
                     }
                     .foregroundColor(.white)

commit 366ae3f82c3949742ad374dca979ffaf0ccda7a1

fixed bug in date filtering introduced in [69e2dcb2]

Finance Tracker/DataModel.swift
@@ -64,19 +64,21 @@ class Data: ObservableObject {
     func refreshBalance() {
         switch defaultTimespan {
         case 1:
-            balance = database.getBalance(startDate: Date().startOfLastMonth(), endDate: Date().endOfLastMonth())
-        case 2:
             balance = database.getBalance(startDate: Date().todayOneMonthAgo(), endDate: Date())
+        case 2:
+            balance = database.getBalance(startDate: Date().startOfLastMonth(), endDate: Date().startOfCurrentMonth())
         case 3:
-            balance = database.getBalance(startDate: Date().startOfMonthThreeMonthsAgo(), endDate: Date().endOfLastMonth())
+            balance = database.getBalance(startDate: Date().startOfMonthThreeMonthsAgo(), endDate: Date().startOfCurrentMonth())
         case 4:
-            balance = database.getBalance(startDate: Date().startOfMonthSixMonthsAgo(), endDate: Date().endOfLastMonth())
+            balance = database.getBalance(startDate: Date().startOfMonthSixMonthsAgo(), endDate: Date().startOfCurrentMonth())
         case 5:
             balance = database.getBalance(startDate: Date().startOfCurrentYear(), endDate: Date())
         case 6:
-            balance = database.getBalance(startDate: Date().startOfLastYear(), endDate: Date().endOfLastYear())
+            balance = database.getBalance(startDate: Date().startOfLastYear(), endDate: Date().startOfCurrentYear())
         case 7:
             balance = database.getBalance(startDate: Date().todayOneYearAgo(), endDate: Date())
+            debugPrint("Date:\(Date().todayOneYearAgo())")
+            debugPrint("Date:\(Date().startOfCurrentYear())")
         default:
             balance = database.getBalance(startDate: Date().startOfCurrentMonth(), endDate: Date())
         }

Finance Tracker/Extensions.swift
@@ -30,15 +30,9 @@ extension Date {
         )!
     }
     
-    func endOfLastMonth() -> Date {
-        return Calendar.current.date(
-            byAdding: DateComponents(month: 1, day: -1), to: self.startOfLastMonth()
-        )!
-    }
-    
     func todayOneMonthAgo() -> Date {
         return Calendar.current.date(
-            byAdding: DateComponents(month: -1), to: Date()
+            byAdding: DateComponents(month: -1), to: Calendar.current.startOfDay(for: self)
         )!
     }
     
@@ -68,15 +62,9 @@ extension Date {
         )!
     }
     
-    func endOfLastYear() -> Date {
-        return Calendar.current.date(
-            byAdding: DateComponents(year: 1, month: -1), to: self.startOfLastYear()
-        )!
-    }
-    
     func todayOneYearAgo() -> Date {
         return Calendar.current.date(
-            byAdding: DateComponents(year: -1), to: Date()
+            byAdding: DateComponents(year: -1), to: Calendar.current.startOfDay(for: self)
         )!
     }
 }