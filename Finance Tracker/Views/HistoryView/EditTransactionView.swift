DannySeidel/Finance-Tracker

commit 24a1768fdd365a9ba4ecefcc3652629dbb0e9b78

added repeat id + dark/light mode improvements

Finance Tracker/Views/HistoryView/EditTransactionView.swift
@@ -20,6 +20,7 @@ struct EditTransactionView: View {
     
     @Binding var showingSheet: Bool
     @Binding var uuid: String
+    @Binding var repeatUuid: String
     @Binding var transactionType: Bool
     
     @State private var showingAlert = false
@@ -44,7 +45,7 @@ struct EditTransactionView: View {
                         DateView(dateAndTime: $dateAndTime, repeatTag: $repeatTag, endRepeat: $endRepeat, repeatEndDate: $repeatEndDate)
                         MapView()
                     }
-                    .background(Color.init(UIColor(named: "AppBackground")!))
+                    .background(Color.init(UIColor(named: "AppSheetBackground")!))
                 }
                 .navigationBarItems(
                     leading:
@@ -65,34 +66,7 @@ struct EditTransactionView: View {
                         },
                     trailing:
                         Button("Save") {
-                            if amount != nil {
-                                if name == nil {
-                                    name = "unnamed Transaction"
-                                }
-                                if category == nil {
-                                    category = "no Category"
-                                }
-                                data.database.updateTransaction(
-                                    transaction: Transaction(
-                                        id: uuid,
-                                        amount: amount! * factor,
-                                        name: name!,
-                                        category: category!,
-                                        dateAndTime: dateAndTime,
-                                        repeatTag: repeatTag,
-                                        endRepeat: endRepeat,
-                                        repeatEndDate: repeatEndDate
-                                    )
-                                )
-                                if storeNewCategoriesByDefault && category != "no Category" {
-                                    transactionType ? data.database.insertIncomeCategory(newCategory: category!) : data.database.insertExpenseCategory(newCategory: category!)
-                                }
-                                data.refreshTransactionGroups()
-                                data.refreshBalance()
-                                showingSheet.toggle()
-                            } else {
-                                showingAlert.toggle()
-                            }
+                            updateTransaction()
                         }
                         .alert(isPresented: $showingAlert) {
                             Alert(title: Text("Missing an amount"))
@@ -101,31 +75,62 @@ struct EditTransactionView: View {
                 .navigationBarTitleDisplayMode(.inline)
                 .foregroundColor(Color.init(UIColor(named: "AppText")!))
             }
-            .onAppear(perform: {
-                transactionType = transactionType
-                amount = transaction.amount * factor
-                name = transaction.name
-                if name == "unnamed Transaction" {
-                    name = nil
-                }
-                category = transaction.category
-                if category == "no Category" {
-                    category = nil
-                }
-                dateAndTime = transaction.dateAndTime
-                repeatTag = transaction.repeatTag
-                endRepeat = transaction.endRepeat
-                repeatEndDate = transaction.repeatEndDate
-            })
+            .onAppear(perform: initializeTransactionVariables)
         }
+    }
+    
+    private func initializeTransactionVariables() {
+        transactionType = transactionType
+        amount = transaction.amount * factor
+        name = transaction.name
+        if name == "unnamed Transaction" {
+            name = nil
+        }
+        category = transaction.category
+        if category == "no Category" {
+            category = nil
+        }
+        dateAndTime = transaction.dateAndTime
+        repeatTag = transaction.repeatTag
+        endRepeat = transaction.endRepeat
+        repeatEndDate = transaction.repeatEndDate
+    }
+    
+    private func updateTransaction() {
+        if amount != nil {
+            if name == nil {
+                name = "unnamed Transaction"
+            }
+            if category == nil {
+                category = "no Category"
+            }
+            data.database.updateTransaction(
+                transaction: Transaction(
+                    id: uuid,
+                    amount: amount! * factor,
+                    name: name!,
+                    category: category!,
+                    dateAndTime: dateAndTime,
+                    repeatTag: repeatTag,
+                    endRepeat: endRepeat,
+                    repeatEndDate: repeatEndDate
+                )
+            )
+            if storeNewCategoriesByDefault && category != "no Category" {
+                transactionType ? data.database.insertIncomeCategory(newCategory: category!) : data.database.insertExpenseCategory(newCategory: category!)
+            }
+            data.refreshTransactionGroups()
+            data.refreshBalance()
+        }
+        showingSheet.toggle()
     }
 }
 
 struct EditTransactionView_Previews: PreviewProvider {
     static var previews: some View {
-        EditTransactionView(showingSheet: .constant(true), uuid: .constant(""), transactionType: .constant(false))
+        EditTransactionView(showingSheet: .constant(true), uuid: .constant(""), repeatUuid: .constant(""), transactionType: .constant(false))
             .environmentObject(Data())
-        EditTransactionView(showingSheet: .constant(true), uuid: .constant(""), transactionType: .constant(false))
+        EditTransactionView(showingSheet: .constant(true), uuid: .constant(""), repeatUuid: .constant(""), transactionType: .constant(false))
             .environmentObject(Data())
             .preferredColorScheme(.dark)
     }

Finance Tracker/Views/TransactionView/AddTransactionView.swift
@@ -63,7 +63,7 @@ struct AddTransactionView: View {
                 )
                 .navigationBarTitleDisplayMode(.inline)
                 .foregroundColor(Color.init(UIColor(named: "AppText")!))
-                .background(Color.init(UIColor(named: "AppBackground")!))
+                .background(Color.init(UIColor(named: "AppSheetBackground")!))
             }
             .alert(isPresented: $showingAlert) {
                 Alert(title: Text("Missing an amount"))
@@ -84,6 +84,7 @@ struct AddTransactionView: View {
                     repeatTag: repeatTag,
                     endRepeat: endRepeat,
                     repeatEndDate: repeatEndDate
+                    
                 )
             )
             if storeNewCategoriesByDefault && category != "no Category" {

Finance Tracker/DataModel.swift
@@ -19,6 +19,7 @@ struct Transaction: Hashable, Identifiable {
     var repeatTag: Int
     var endRepeat: Bool
     var repeatEndDate: Date
+    var repeatId = UUID().uuidString
 }
 
 
@@ -28,6 +29,7 @@ struct HistoryTransaction: Hashable, Identifiable {
     var name: String
     var category: String
     var dateAndTime: Date
+    var repeatId: String
 }
 
 

Finance Tracker/Database.swift
@@ -51,6 +51,7 @@ class Database {
     let repeatTag = Expression<Int>("repeatTag")
     let endRepeat = Expression<Bool>("endRepeat")
     let repeatEndDate = Expression<Date>("repeatEndDate")
+    let repeatId = Expression<String>("repeatId")
     
     let nameTable = Table("names")
     
@@ -80,6 +81,7 @@ class Database {
             table.column(repeatTag)
             table.column(endRepeat)
             table.column(repeatEndDate)
+            table.column(repeatId)
         }
             
         let createNameTable = nameTable.create(ifNotExists: true) { table in
@@ -112,8 +114,8 @@ class Database {
             categoriesIncome.forEach { category in
                 insertIncomeCategory(newCategory: category)
             }
+            firstUse = false
         }
-        firstUse = false
     }
     
     
@@ -200,7 +202,8 @@ class Database {
             dateAndTime <- transaction.dateAndTime,
             repeatTag <- transaction.repeatTag,
             endRepeat <- transaction.endRepeat,
-            repeatEndDate <- transaction.repeatEndDate
+            repeatEndDate <- transaction.repeatEndDate,
+            repeatId <- transaction.repeatId
         )
         do {
             try db.run(insert)
@@ -228,7 +231,7 @@ class Database {
         var transactions: [HistoryTransaction] = []
         do {
             let transactionRows = Array(try db.prepare(transactionTable
-                                                        .select(id, amount, name, category, dateAndTime)
+                                                        .select(id, amount, name, category, dateAndTime, repeatId)
                                                         .order(dateAndTime.desc)
                                                       ))
             for transactionRow in transactionRows {

Finance Tracker/Extensions.swift
@@ -101,6 +101,7 @@ extension Transaction {
             try self.repeatTag = row.get(Expression<Int>("repeatTag"))
             try self.endRepeat = row.get(Expression<Bool>("endRepeat"))
             try self.repeatEndDate = row.get(Expression<Date>("repeatEndDate"))
+            try self.repeatId = row.get(Expression<String>("repeatId"))
         } catch {
             self.id = ""
             self.amount = 1.0
@@ -110,6 +111,7 @@ extension Transaction {
             self.repeatTag = 0
             self.endRepeat = false
             self.repeatEndDate = Date.now
+            self.repeatId = ""
             print(error)
         }
     }
@@ -124,12 +126,14 @@ extension HistoryTransaction {
             try self.name = row.get(Expression<String>("name"))
             try self.category = row.get(Expression<String>("category"))
             try self.dateAndTime = row.get(Expression<Date>("dateAndTime"))
+            try self.repeatId = row.get(Expression<String>("repeatId"))
         } catch {
             self.id = ""
             self.amount = 1.0
             self.name = ""
             self.category = ""
             self.dateAndTime = Date.now
+            self.repeatId = ""
             print(error)
         }
     }

Finance Tracker/Views/HistoryView/HistoryView.swift
@@ -13,6 +13,7 @@ struct HistoryView: View {
     @State var filterTag = 0
     @State var showingSheet = false
     @State var uuid = ""
+    @State var repeatUuid = ""
     @State var transactionType = false
     
     var searchGroups: [[HistoryTransaction]] {
@@ -68,7 +69,7 @@ struct HistoryView: View {
                                             Label("Edit", systemImage: "pencil")
                                         }
                                         Button(role: .destructive) {
-                                            onDelete(id: transaction.id)
+                                            onDelete(id: transaction.id, repeatId: transaction.repeatId)
                                         } label: {
                                             Label("Delete", systemImage: "trash")
                                         }
@@ -81,7 +82,7 @@ struct HistoryView: View {
                 .searchable(text: $transactionSearchName, placement: .navigationBarDrawer(displayMode: .always))
             }
             .sheet(isPresented: $showingSheet) {
-                EditTransactionView(showingSheet: $showingSheet, uuid: $uuid, transactionType: $transactionType)
+                EditTransactionView(showingSheet: $showingSheet, uuid: $uuid, repeatUuid: $repeatUuid, transactionType: $transactionType)
             }
             .navigationTitle("History")
             .navigationBarTitleDisplayMode(.inline)
@@ -104,7 +105,7 @@ struct HistoryView: View {
         .background(Color.init(UIColor(named: "AppBackground")!))
     }
     
-    private func onDelete(id: String) {
+    private func onDelete(id: String, repeatId: String) {
         data.database.deleteTransaction(uuid: id)
         data.refreshTransactionGroups()
         data.refreshBalance()
@@ -116,12 +117,12 @@ struct HistoryView_Previews: PreviewProvider {
         NavigationView {
             HistoryView(showingSheet: false)
                 .environmentObject(Data())
-            TransactionElement(transaction: HistoryTransaction(id: "1959addc-387b-437c-87d2-776a40e9f509", amount: 9.11, name: "Lunch", category: "", dateAndTime: Date.now))
+            TransactionElement(transaction: HistoryTransaction(id: "1959addc-387b-437c-87d2-776a40e9f509", amount: 9.11, name: "Lunch", category: "", dateAndTime: Date.now, repeatId: ""))
         }
         NavigationView {
             HistoryView(showingSheet: false)
                 .environmentObject(Data())
-            TransactionElement(transaction: HistoryTransaction(id: "1959addc-387b-437c-87d2-776a40e9f509", amount: 9.11, name: "Lunch", category: "", dateAndTime: Date.now))
+            TransactionElement(transaction: HistoryTransaction(id: "1959addc-387b-437c-87d2-776a40e9f509", amount: 9.11, name: "Lunch", category: "", dateAndTime: Date.now, repeatId: ""))
         }
         .preferredColorScheme(.dark)
     }

Finance Tracker/Views/HistoryView/TransactionElement.swift
@@ -51,18 +51,18 @@ struct TransactionElement_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
             Spacer()
-            TransactionElement(transaction: HistoryTransaction(id: "1959addc-387b-437c-87d2-776a40e9f509", amount: 9.11, name: "Lunch", category: "Food", dateAndTime: Date.now))
+            TransactionElement(transaction: HistoryTransaction(id: "1959addc-387b-437c-87d2-776a40e9f509", amount: 9.11, name: "Lunch", category: "Food", dateAndTime: Date.now, repeatId: ""))
                 .frame(width: 400, height: 75)
-            TransactionElement(transaction: HistoryTransaction(id: "53ef26c9-009b-4b41-ba66-99405bf775d9", amount: -15.73, name: "Dinner", category: "Restaurant", dateAndTime: Date.now))
+            TransactionElement(transaction: HistoryTransaction(id: "53ef26c9-009b-4b41-ba66-99405bf775d9", amount: -15.73, name: "Dinner", category: "Restaurant", dateAndTime: Date.now, repeatId: ""))
                 .environmentObject(Data())
                 .frame(width: 400, height: 75)
             Spacer()
         }
         .background(Color.init(UIColor(named: "AppBackground")!))
         VStack {
-            TransactionElement(transaction: HistoryTransaction(id: "1959addc-387b-437c-87d2-776a40e9f509", amount: 9.11, name: "Lunch", category: "Food", dateAndTime: Date.now))
+            TransactionElement(transaction: HistoryTransaction(id: "1959addc-387b-437c-87d2-776a40e9f509", amount: 9.11, name: "Lunch", category: "Food", dateAndTime: Date.now, repeatId: ""))
                 .frame(width: 400, height: 75)
-            TransactionElement(transaction: HistoryTransaction(id: "53ef26c9-009b-4b41-ba66-99405bf775d9", amount: -15.73, name: "Dinner", category: "Restaurant", dateAndTime: Date.now))
+            TransactionElement(transaction: HistoryTransaction(id: "53ef26c9-009b-4b41-ba66-99405bf775d9", amount: -15.73, name: "Dinner", category: "Restaurant", dateAndTime: Date.now, repeatId: ""))
                 .environmentObject(Data())
                 .frame(width: 400, height: 75)
                 .preferredColorScheme(.dark)

Finance Tracker/Views/SettingsView/EditCategoryView.swift
@@ -50,7 +50,7 @@ struct EditCategoryView: View {
                     }
                     .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
                 }
-                .background(Color.init(UIColor(named: "AppBackground")!))
+                .background(Color.init(UIColor(named: "AppSheetBackground")!))
                 Button("Add Category") {
                     addCategoryAlert.toggle()
                 }

commit f7815016c2a309df2e687ba54902d87b524c324f

fixed bug introduced in [24a1768] + alert not dismissing itself

Finance Tracker/Views/HistoryView/EditTransactionView.swift
@@ -121,8 +121,10 @@ struct EditTransactionView: View {
             }
             data.refreshTransactionGroups()
             data.refreshBalance()
+            showingSheet.toggle()
+        } else {
+            showingAlert.toggle()
         }
-        showingSheet.toggle()
     }
 }
 

Finance Tracker/Views/TransactionView/AddTransactionView.swift
@@ -90,12 +90,12 @@ struct AddTransactionView: View {
             if storeNewCategoriesByDefault && category != "no Category" {
                 transactionType ? data.database.insertIncomeCategory(newCategory: category!) : data.database.insertExpenseCategory(newCategory: category!)
             }
+            data.refreshBalance()
+            data.refreshTransactionGroups()
+            showTransactionSheet.toggle()
         } else {
             showingAlert.toggle()
         }
-        data.refreshBalance()
-        data.refreshTransactionGroups()
-        showTransactionSheet.toggle()
     }
 }
 